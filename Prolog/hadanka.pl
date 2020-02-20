1bf6663d896c13defff395940ef9bb3e097fdf81
Vít Šefl <vituscze@gmail.com>
2020-02-20 22:39:40 +0100

    ...

---
+++ Prolog/hadanka.pl
@@ -0,0 +1,16 @@
+% Oženil jsem se s jednou vdovou, která již měla dospělou dceru.
+% Můj otec, který nás často navštěvoval, se do mé nevlastní dcery
+% zamiloval a vzal si ji za ženu. Tak se můj otec stal mým zeťem a
+% moje nevlastní dcera mojí nevlastní matkou. Za několik měsíců
+% porodila má žena syna, který se stal švagrem mého otce a současně
+% mým strýcem. Manželce mého otce se také narodil syn, který se tak
+% stal mým bratrem a současně vnukem.
+%
+% Vytvořte v Prologu databázi, která reprezentuje fakta v této hádance.
+% Poté definujte predikáty pro rodinné vztahy (švagr, bratr, vnuk, atp)
+% a ověřte, že skutečně platí.
+%
+% Nakonec definuje predikát "dědeček" a použijte ho na zodpovězení
+% následující otázky:
+%
+% Jsem svým vlastním dědečkem?
