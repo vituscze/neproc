0d1028af44c8aca141418ea752bcf96f8cbbe0d5
Vít Šefl <vituscze@gmail.com>
2023-04-05 12:31:35 +0200

    update hw4.hs

--- Homework/hw4.hs
+++ Homework/hw4.hs
@@ -15,6 +15,8 @@ rleDecode :: [(Int, a)] -> [a]
 rleDecode = undefined
 
 -- 2) Definujte nekonečný seznam všech prvočísel. Pokuste se o efektivní řešení.
+-- Poté pomocí něj definujte funkci, která v daném rozsahu najde dvojici po sobě
+-- jdoudích prvočísel s maximálním rozdílem. Pokud je jich více, vrátí první z nich.
 
 -- >>> take 5 primes
 -- [2,3,5,7,11]
@@ -22,21 +24,35 @@ rleDecode = undefined
 primes :: [Integer]
 primes = undefined
 
--- 3) Implementujte mergesort.
+-- >>> gap 1000
+-- (887, 907)
+--
+gap :: Int -> (Int, Int)
+gap = undefined
 
-mergeWith :: (a -> a -> Bool) -> [a] -> [a] -> [a]
+-- Prvním argumentem je konec rozsahu, začátek bude vždy 2. Můžete předpokládat,
+-- že konec bude alespoň 3.
+
+-- 3) Implementujte mergesort, který vyhazuje duplikáty.
+
+mergeWith :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
 mergeWith = undefined
 
-sortWith  :: (a -> a -> Bool) -> [a] -> [a]
+sortWith  :: (a -> a -> Ordering) -> [a] -> [a]
 sortWith = undefined
 
 -- Prvním argumentem je funkce, která provádí porovnávání.
+-- Ordering je datový typ, který obsahuje 3 konstanty: LT, EQ, GT
+-- (less than, equal, greater than).
 --
--- >>> sortWith (<) [10,9..1]
+-- >>> sortWith compare [10,9..1]
 -- [1,2,3,4,5,6,7,8,9,10]
 --
--- >>> sortWith (>) [10,9..1]
+-- >>> sortWith (flip compare) [10,9..1]
 -- [10,9,8,7,6,5,4,3,2,1]
+--
+-- >>> sortWith compare [1,1,1]
+-- [1]
 --
 -- BONUS)
 --
